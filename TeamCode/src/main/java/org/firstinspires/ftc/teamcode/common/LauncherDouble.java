688246b7a0fc80dfbd6514dc2d84718fd75b9d9b
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-11-05 14:30:27 -0500

    adsFG

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/LauncherDouble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/LauncherDouble.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import com.qualcomm.robotcore.hardware.VoltageSensor;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data.MotorData;
 import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data.LauncherData;
@@ -13,23 +13,29 @@ import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data.Launcher
 public class LauncherDouble extends Component {
     private final DcMotorEx launcherMotorL, launcherMotorR;
     private final LauncherData launcherData = new LauncherData();
-    private final double maxPower;
+    private final double maxVelocityFactor, targetOperatingVoltage;
+    private double currentVoltage, voltageCompensationFactor;
     private final int maxTicksPerSecond;
     int targetVelocity = 0;
+    private VoltageSensor myControlHubVoltageSensor;
 
     public LauncherDouble(HardwareMap hardwareMap, Telemetry telemetry, String motorNameL, String motorNameR, MotorData motorData) {
         super(telemetry);
-        maxPower = launcherData.maxPower;
-        maxTicksPerSecond = motorData.maxTicksPerSec;
+        myControlHubVoltageSensor = hardwareMap.get(VoltageSensor.class, "Control Hub");
+        targetOperatingVoltage = launcherData.targetOperatingVoltage;
+        currentVoltage =  myControlHubVoltageSensor.getVoltage();
+        voltageCompensationFactor = targetOperatingVoltage/currentVoltage;
 
+        maxVelocityFactor = launcherData.maxVelocityFactor;
+        maxTicksPerSecond = motorData.maxTicksPerSec;
         launcherMotorL = hardwareMap.get(DcMotorEx.class, motorNameL);
         launcherMotorR = hardwareMap.get(DcMotorEx.class, motorNameR);
         resetEncoders();
-        setVelocityFactor(targetVelocity);
-        setMotorsPower(maxPower);
+        setVelocityFactorVoltageCompensated(targetVelocity);
     }
 
-    public void setVelocityFactor(double targetVelocityFactor) {
+    public void setVelocityFactorVoltageCompensated(double targetVelocityFactor) {
+
         if (Math.abs(targetVelocityFactor) < 0.05)
         {
             targetVelocityFactor = 0.0;
@@ -40,7 +46,9 @@ public class LauncherDouble extends Component {
         {
             targetVelocityFactor = -1.0;
         }
-        targetVelocity = Math.toIntExact(Math.round(targetVelocityFactor * maxTicksPerSecond));
+        currentVoltage = myControlHubVoltageSensor.getVoltage();
+        voltageCompensationFactor = targetOperatingVoltage/currentVoltage;
+        targetVelocity = Math.toIntExact(Math.round(targetVelocityFactor * maxTicksPerSecond *voltageCompensationFactor));
         setMotorsTargetVelocity(targetVelocity);
     }
 
@@ -71,6 +79,10 @@ public class LauncherDouble extends Component {
 
     public void log() {
         telemetry.addData("maxTicksPerSecond:  ", maxTicksPerSecond);
+        telemetry.addData("targetOperatingVoltage:  ", targetOperatingVoltage);
+        telemetry.addData("currentVoltage:  ", currentVoltage);
+        telemetry.addData("voltageCompensationFactor: ", voltageCompensationFactor);
+    //    telemetry.addData("targetVelocityFactor: ", targetVelocityFactor);
         telemetry.addData("targetVelocity:  ", targetVelocity);
         telemetry.addData("Actual Velocity L:  ", launcherMotorL.getVelocity());
         telemetry.addData("Actual Velocity R:  ", launcherMotorR.getVelocity());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/LauncherDoubleTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/LauncherDoubleTest.java
@@ -22,7 +22,7 @@ public class LauncherDoubleTest extends LinearOpMode {
     public void runOpMode() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         shooter = new LauncherDouble(hardwareMap, telemetry, "shooterL", "shooterR", new GoBilda6000DcMotorData());
-        shooter.setVelocityFactor(targetVelocityFactor);
+        shooter.setVelocityFactorVoltageCompensated(targetVelocityFactor);
 
         waitForStart();
         while (opModeIsActive() && !isStopRequested()) {
@@ -31,7 +31,7 @@ public class LauncherDoubleTest extends LinearOpMode {
             } else if (gamepad1.a) {
                 targetVelocityFactor -= velocityFactorIncrement;
             }
-            shooter.setVelocityFactor(targetVelocityFactor);
+            shooter.setVelocityFactorVoltageCompensated(targetVelocityFactor);
             telemetry.addData("targetVelocityFactor in ShooterDoubleTest: ", targetVelocityFactor);
             shooter.log();
             sleep(100);
