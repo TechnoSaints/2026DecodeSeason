2cd76fc0536d2685507eddbe58cc46d1882eabf2
Henderson <hendersonc@trinityprep.org>
2025-11-14 10:13:16 -0500

    Many updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/RollerCRServo.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.common;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data.GoBilda435DcMotorData;
+import org.firstinspires.ftc.teamcode.common.hardwareConfiguration.data.MotorData;
+
+@Config
+public class RollerCRServo extends Component {
+    private final CRServo servo;
+    private double onPower = 1.0;
+    public RollerCRServo(HardwareMap hardwareMap, Telemetry telemetry, String servoName) {
+        super(telemetry);
+
+        servo = hardwareMap.get(CRServo.class, servoName);
+    }
+    public void forward()
+    {
+        servo.setPower(onPower);
+    }
+
+    public void reverse()
+    {
+        servo.setPower(-onPower);
+    }
+
+    public void stop()
+    {
+        servo.setPower(0.0);
+    }
+}
