94bc3b8f1bd01574da2b50e861408839e7d4ff9d
TechnoSaints <130092700+TechnoSaints@users.noreply.github.com>
2025-12-10 11:18:21 -0500

    Update ShortRedAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/ShortRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/ShortRedAuto.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.opmode.FieldLocations;
 import org.firstinspires.ftc.teamcode.opmode.Paths;
 
-@Autonomous(name = "shortRedAuto", group = "Red")
+@Autonomous(name = "\uD83D\uDD34shortRedAuto", group = "Red")
 //@Disabled
 public class ShortRedAuto extends AutoOpMode {
 
