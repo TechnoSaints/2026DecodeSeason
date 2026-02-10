35b607f1a89895d44d71a2ce0a8c815855e973ad
puppyshark7 <94720704+puppyshark7@users.noreply.github.com>
2026-02-10 17:21:08 -0500

    Add follower to Limelight test and update Gradle
    Integrates a Follower into the Limelight test opmode: adds pose conversion, follower setup/update, timer, improved Limelight result handling and telemetry, and re-enables the opmode (removed @Disabled). Adjusts Limelight pipeline/start sequence. Updates Android Gradle settings (moves targetSdk handling to lint/testOptions and sets namespace) and expands gradle.properties Android flags. Bumps Gradle wrapper to 9.1.0.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/limeLightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/test/limeLightTest.java
@@ -1,91 +1,89 @@
 package org.firstinspires.ftc.teamcode.opmode.test;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.ftc.InvertedFTCCoordinates;
+import com.pedropathing.ftc.PoseConverter;
+import com.pedropathing.geometry.PedroCoordinates;
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Config
-@Disabled
 @TeleOp(name = "Limelight Test", group = "Linear OpMode")
 public class limeLightTest extends LinearOpMode {
 
     private Limelight3A limelight;
+    private Follower follower;
+    private ElapsedTime timer;
+    boolean limelightActive = false;
+
+    double limelightMountAngleDegrees = 25.0;
+
+    double limelightLensHeightInches = 16.0;
+
+    double angleToGoalDegrees;
+
+    double angleToGoalRadians;
+
+    double distanceFromLimelightToGoalInches;
+
+
 
     @Override
     public void runOpMode() throws InterruptedException
     {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        follower = Constants.createFollower(hardwareMap);
+        timer = new ElapsedTime(30);
 
         telemetry.setMsTransmissionInterval(11);
 
-        limelight.pipelineSwitch(8);
+        limelight.pipelineSwitch(0);
+        limelight.start();
 
         /*
          * Starts polling for data.
          */
-        limelight.start();
-
-
         waitForStart();
 
-        while (opModeIsActive()) {
-            LLResult result = limelight.getLatestResult();
-//            NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-//            NetworkTableEntry ty = table.getEntry("ty");
-//            double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+        while (opModeIsActive() && !isStopRequested()) {
+            if (true) {
+                LLResult result = limelight.getLatestResult();
+                if (result != null) {
+                    if (result.isValid()) {
+                        Pose3D pose3D = result.getBotpose();
+                        Position botPose = pose3D.getPosition().toUnit(DistanceUnit.INCH);
+                      //  angleToGoalDegrees = limelightMountAngleDegrees + result.getTy();
 
-            // ^ No need for networktable
+                        telemetry.addData("tx", result.getTx());
+                        telemetry.addData("ty", result.getTy());
+                        telemetry.addData("Bot Pose", botPose.toString());
+                        telemetry.addData("Heading", pose3D.getOrientation().getYaw(AngleUnit.DEGREES));
 
-            double targetOffsetAngle_Vertical = result.getTy();
-            double targetOffsetAngle_Horizontal = result.getTx();
-
-            double perfectAlignmentOffset = 3;
-            double movmentNeededX = 1000;
-            if (Math.abs(targetOffsetAngle_Horizontal) > perfectAlignmentOffset) {
-                if (targetOffsetAngle_Horizontal > 0) { // limelight is to the right of bot
-                    // Move the bot right
-                    while (Math.abs(targetOffsetAngle_Horizontal) > perfectAlignmentOffset) {
-
+                        Pose2D pose2D = new Pose2D(DistanceUnit.INCH, botPose.x, botPose.y, AngleUnit.DEGREES, pose3D.getOrientation().getYaw(AngleUnit.DEGREES));
+                        Pose followerPose = PoseConverter.pose2DToPose(pose2D, InvertedFTCCoordinates.INSTANCE);
+                        followerPose.getAsCoordinateSystem(PedroCoordinates.INSTANCE);
+                        follower.setPose(followerPose);
+                        timer.reset();
                     }
-                } else if (targetOffsetAngle_Horizontal < 0) { // limelight is to the left of bot
-                    // Move the bot left
-                }
-            } else {
-                movmentNeededX = targetOffsetAngle_Horizontal;
-            }
-            movmentNeededX = targetOffsetAngle_Horizontal;
-
-            // how many degrees back is your limelight rotated from perfectly vertical?
-            double limelightMountAngleDegrees = 25.0;
-
-            // distance from the center of the Limelight lens to the floor
-            double limelightLensHeightInches = 20.0;
-
-            // distance from the target to the floor
-            double goalHeightInches = 60.0;
-
-            double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-            double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-
-            //calculate distance
-            double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
-            if (result != null) {
-                if (result.isValid()) {
-                    Pose3D botpose = result.getBotpose();
-                    telemetry.addData("tx", result.getTx());
-                    telemetry.addData("ty", result.getTy());
-                    telemetry.addData("Botpose", botpose.toString());
-                    telemetry.addData("Distance", distanceFromLimelightToGoalInches);
-                    telemetry.addData("Angle Off", movmentNeededX);
-                    telemetry.update();
                 }
             }
+            follower.update();
+            telemetry.addData("Follower Pose", follower.getPose().toString());
+            telemetry.update();
         }
     }
 }
